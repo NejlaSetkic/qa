be1d683c69daf48a29733a9716d6d65b37094e60
NejlaSetkic <99647005+NejlaSetkic@users.noreply.github.com>
2024-03-20 14:44:32 +0100

    resolve #100

--- src/Blue.tsx
+++ src/Blue.tsx
@@ -1,12 +1,1 @@
-import {AbsoluteFill} from 'remotion';
-
-export const Blue: React.FC = () => {
-	return (
-		<AbsoluteFill
-			style={{
-				background: 'linear-gradient(to right, #4290f5, #42e9f5)',
-				boxShadow: '0 0 10px rgba(0, 0, 0, 0.7)',
-			}}
-		/>
-	);
-};
+BLABLA
